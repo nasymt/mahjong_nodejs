913079b7d05243d43d6b273c576efb88c5be5c60
mtok <TadashiMatsuoka@MacBook-Pro.local>
2016-03-06 00:42:01 +0900

    select Bakaze only(local)

---
+++ app.js
@@ -0,0 +1,48 @@
+/*var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);*/
+var express = require('express'),
+app = express(),
+http = require('http').Server(app),
+io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  
+  socket.on('setup',function(data){
+  	socket.join(data.room);
+  	
+  	var baName;
+  	switch(data.room){
+  		case "ton":
+  			baName="東";
+  			break;
+  		case "nan":
+  			baName="南";
+  			break;
+  		case "sha":
+  			baName="西";
+  			break;
+  		case "pei":
+  			baName="北";
+  			break;
+  	}
+  	socket.emit('notice_bakaze',"あなたは"+baName+"です。");
+  	console.log("あなたは"+baName+"です。" );
+  });
+  
+});
+
+//http.listen(process.env.PORT||3000 , function(){
+http.listen(3000 , function(){
+  console.log('listening on *:3000');
+});
